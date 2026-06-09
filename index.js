66801d0f9ee651f18d3832bb6334678a48ca43b5
Jaspreet Singh <95354014+withjass19@users.noreply.github.com>
2026-06-09 21:31:33 +0530

    .

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-const express = require("express")
+require("dotenv").config();
+const express = require("express");
+const { mongoose } = require("mongoose");
 
 const app = express();
 
-const PORT = 5000;
-
 app.use(express.json());
+
+let isConnected = false;
+
+async function connectToMongoDB(){
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        isConnected = true;
+        console.log('connected to MongoDB')
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+app.use((req, res, next) => {
+    if(!isConnected){
+        connectToMongoDB();
+    }
+    next();
+})
 
 app.get("/", (req, res) => {
     res.send("hello world");
